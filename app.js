6d809935fd56ac8e8c15eb73db874f6732bdf001
Akane Kano <bglakane@gmail.com>
2024-03-14 09:56:51 +0900

    adding pokemon name

--- app.js
+++ app.js
@@ -3,13 +3,18 @@ const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprite
 const ball = document.querySelector("#ball");
 let i = 0;
 const newImg = document.querySelector('#pokemon');
+const pokemonNames = ['???', 'Bulbasaur（フシギダネ）', 'Ivysaur（フシギソウ）', 'Venusaur（フシギバナ）', 'Charmander（ヒトカゲ）', 'Charmeleon（リザード）', 'Charizard:リザードン'];
+const newText = document.querySelector('#pokemonText');
 
 ball.addEventListener("click", () => {
   i++
-  if (i > 151) {
+  if (i > 6) {
     i = 0;
   }
-  newImg.src = `${baseURL}${i}.png`
+  newText.innerText = pokemonNames[i];
+  newImg.src = `${baseURL}${i}.png`;
   newImg.appendChild(newImg);
+
 });
+
 
